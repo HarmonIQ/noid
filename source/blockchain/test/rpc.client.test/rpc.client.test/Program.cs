c16248a6aae158e6218ffbc7750179bf4187fdfd
AmirAbrams <aabrams@myharmoniq.com>
2016-11-15 03:05:24 -0600

    [BlockChain] Add test project that connects to blockchain JSON RPC

---
+++ source/blockchain/test/rpc.client.test/rpc.client.test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Blockchain.RPC;
+
+namespace noid.blockchain.client.test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = "";
+
+            json = RPCClient.CallBlockchainRPC(Chain.Sequence, SequenceRpcMethods.getinfo, null);
+            Console.WriteLine(json);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ source/blockchain/test/rpc.client.test/rpc.client.test/rpcclient.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Newtonsoft.Json;
+using System.Text;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Blockchain.RPC
+{
+    static class RPCClient
+    {
+        private static void SetBasicAuthHeader(WebRequest webRequest, string username, string password)
+        {
+            var authInfo = username + ":" + password;
+            authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
+            webRequest.Headers["Authorization"] = "Basic " + authInfo;
+        }
+
+        static public string CallBlockchainRPC(Chain chain, SequenceRpcMethods rpcMethod, params object[] parameters)
+        {
+            string URL = "http://localhost:16663";
+            string username = "fekSZZMb";
+            string password = "6iFPVw0bRWGhtNiZ";
+            int RpcRequestTimeoutInSeconds = 3000;
+
+            var jsonRpcRequest = new JsonRpcRequest(1, rpcMethod.ToString(), parameters);
+            var webRequest = (HttpWebRequest)WebRequest.Create(URL);
+            SetBasicAuthHeader(webRequest, username, password);
+            webRequest.Credentials = new NetworkCredential(username, password);
+            webRequest.ContentType = "application/json-rpc";
+            webRequest.Method = "POST";
+            webRequest.Proxy = null;
+            webRequest.Timeout = RpcRequestTimeoutInSeconds;
+            var byteArray = jsonRpcRequest.GetBytes();
+            webRequest.ContentLength = jsonRpcRequest.GetBytes().Length;
+
+            try
+            {
+                using (var dataStream = webRequest.GetRequestStream())
+                {
+                    dataStream.Write(byteArray, 0, byteArray.Length);
+                    dataStream.Dispose();
+                }
+            }
+            catch (Exception exception)
+            {
+                throw new RpcException("There was a problem sending the request to the wallet", exception);
+            }
+
+            try
+            {
+                string json;
+
+                using (var webResponse = webRequest.GetResponse())
+                {
+                    using (var stream = webResponse.GetResponseStream())
+                    {
+                        using (var reader = new StreamReader(stream))
+                        {
+                            var result = reader.ReadToEnd();
+                            reader.Dispose();
+                            json = result;
+                        }
+                    }
+                }
+                return json.Replace("\n", string.Empty).Replace("\r", string.Empty);
+            }
+            catch (WebException webException)
+            {
+                #region RPC Internal Server Error (with an Error Code)
+
+                var webResponse = webException.Response as HttpWebResponse;
+
+                if (webResponse != null)
+                {
+                    switch (webResponse.StatusCode)
+                    {
+                        case HttpStatusCode.InternalServerError:
+                            {
+                                using (var stream = webResponse.GetResponseStream())
+                                {
+                                    if (stream == null)
+                                    {
+                                        throw new RpcException("The RPC request was either not understood by the server or there was a problem executing the request", webException);
+                                    }
+
+                                    using (var reader = new StreamReader(stream))
+                                    {
+                                        var result = reader.ReadToEnd();
+                                        reader.Dispose();
+
+                                        try
+                                        {
+                                            var jsonRpcResponseObject = JsonConvert.DeserializeObject<JsonRpcResponse<object>>(result);
+
+                                            var internalServerErrorException = new RpcInternalServerErrorException(jsonRpcResponseObject.Error.Message, webException)
+                                            {
+                                                RpcErrorCode = jsonRpcResponseObject.Error.Code
+                                            };
+
+                                            throw internalServerErrorException;
+                                        }
+                                        catch (JsonException)
+                                        {
+                                            throw new RpcException(result, webException);
+                                        }
+                                    }
+                                }
+                            }
+
+                        default:
+                            throw new RpcException("The RPC request was either not understood by the server or there was a problem executing the request", webException);
+                    }
+                }
+
+                #endregion
+
+                #region RPC Time-Out
+
+                if (webException.Message == "The operation has timed out")
+                {
+                    throw new RpcRequestTimeoutException(webException.Message);
+                }
+
+                #endregion
+
+                throw new RpcException("An unknown web exception occured while trying to read the JSON response", webException);
+            }
+            catch (JsonException jsonException)
+            {
+                throw new RpcResponseDeserializationException("There was a problem deserializing the response from the wallet", jsonException);
+            }
+            catch (ProtocolViolationException protocolViolationException)
+            {
+                throw new RpcException("Unable to connect to the server", protocolViolationException);
+            }
+            catch (Exception exception)
+            {
+                var queryParameters = jsonRpcRequest.Parameters.Cast<string>().Aggregate(string.Empty, (current, parameter) => current + (parameter + " "));
+                throw new Exception($"A problem was encountered while calling MakeRpcRequest() for: {jsonRpcRequest.Method} with parameters: {queryParameters}. \nException: {exception.Message}");
+            }
+        }
+
+    }
+
+    public class JsonRpcError
+    {
+        [JsonProperty(PropertyName = "code")]
+        public RpcErrorCode Code { get; set; }
+
+        [JsonProperty(PropertyName = "message")]
+        public string Message { get; set; }
+    }
+
+    public class JsonRpcResponse<T>
+    {
+        [JsonProperty(PropertyName = "result")]
+        public T Result { get; set; }
+
+        [JsonProperty(PropertyName = "error")]
+        public JsonRpcError Error { get; set; }
+
+        [JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+    }
+
+    public class JsonRpcRequest
+    {
+        public JsonRpcRequest(int id, string method, params object[] parameters)
+        {
+            Id = id;
+            Method = method;
+            Parameters = parameters?.ToList() ?? new List<object>();
+        }
+
+        [JsonProperty(PropertyName = "method", Order = 0)]
+        public string Method { get; set; }
+
+        [JsonProperty(PropertyName = "params", Order = 1)]
+        public IList<object> Parameters { get; set; }
+
+        [JsonProperty(PropertyName = "id", Order = 2)]
+        public int Id { get; set; }
+
+        public byte[] GetBytes()
+        {
+            var json = JsonConvert.SerializeObject(this);
+            return Encoding.UTF8.GetBytes(json);
+        }
+    }
+
+    [Serializable]
+    public class RpcException : Exception
+    {
+        public RpcException()
+        {
+        }
+
+        public RpcException(string customMessage) : base(customMessage)
+        {
+        }
+
+        public RpcException(string customMessage, Exception exception) : base(customMessage, exception)
+        {
+        }
+    }
+
+    [Serializable]
+    public class RpcResponseDeserializationException : Exception
+    {
+        public RpcResponseDeserializationException()
+        {
+        }
+
+        public RpcResponseDeserializationException(string customMessage) : base(customMessage)
+        {
+        }
+
+        public RpcResponseDeserializationException(string customMessage, Exception exception) : base(customMessage, exception)
+        {
+        }
+    }
+
+    [Serializable]
+    public class RpcRequestTimeoutException : Exception
+    {
+        public RpcRequestTimeoutException()
+        {
+        }
+
+        public RpcRequestTimeoutException(string customMessage) : base(customMessage)
+        {
+        }
+
+        public RpcRequestTimeoutException(string customMessage, Exception exception) : base(customMessage, exception)
+        {
+        }
+    }
+
+    [Serializable]
+    public class RpcInternalServerErrorException : Exception
+    {
+        public RpcInternalServerErrorException()
+        {
+        }
+
+        public RpcInternalServerErrorException(string customMessage) : base(customMessage)
+        {
+        }
+
+        public RpcInternalServerErrorException(string customMessage, Exception exception) : base(customMessage, exception)
+        {
+        }
+
+        public RpcErrorCode? RpcErrorCode { get; set; }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+            {
+                throw new ArgumentNullException("info");
+            }
+
+            info.AddValue("RpcErrorCode", RpcErrorCode, typeof(RpcErrorCode));
+            base.GetObjectData(info, context);
+        }
+    }
+}
