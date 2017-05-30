917e140ecb72cd6621bf10e13f529233ac27dd90
Amir Abrams <aabrams@myharmoniq.com>
2017-05-30 06:37:24 -0500

    [FHIR] Update Patient FHIR profile library

--- source/noid.fhir.profile/PatientProfile.cs
+++ source/noid.fhir.profile/PatientProfile.cs
@@ -3,18 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using Hl7.Fhir.Rest;
 using Hl7.Fhir.Model;
+using NoID.Cryptographic.Hash;
 
 namespace NoID
 {
     public class PatientProfile
     {
-        private string _languageSelected = "";
-        private Patient pt = new Patient();
+        private readonly Uri fhirAddress = new Uri("http://localhost/spark/fhir");
 
-        public string LanguageHash
+        private string _languageSelected = "";
+        private string _firstName = "";
+        private string _lastName = "";
+        private string _middleName = "";
+
+        private Patient pt = new Patient();
+        //TODO: load and use saltList from matching hubs
+        private string tempSalt = "C560325F-6617-4FE9-BF30-04E67D591637";
+        private List<string> saltList;
+
+        HashWriter.ArgonParams argonParams = new HashWriter.ArgonParams(2, 8192, 4);
+
+        public string Language_Hash
         {
-            get { return _languageSelected; }
+            get { return HashWriter.Hash(_languageSelected, tempSalt, argonParams); }
         }
 
+        public string FirstName_Hash
+        {
+            get { return HashWriter.Hash(_firstName, tempSalt, argonParams); }
+        }
+ 
+        public string LastName_Hash
+        {
+            get { return HashWriter.Hash(_lastName, tempSalt, argonParams); }
+        }
+
+        public string MiddleName_Hash
+        {
+            get { return HashWriter.Hash(_middleName, tempSalt, argonParams); }
+        }
     }
 }
