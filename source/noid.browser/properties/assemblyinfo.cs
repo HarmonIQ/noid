5bdeea64dd709dbb962b27ca4cc31d651190e0fb
Amir Abrams <aabrams@myharmoniq.com>
2017-05-30 02:07:00 -0500

    [UI] Update assembly information

--- source/noid.browser/properties/assemblyinfo.cs
+++ source/noid.browser/properties/assemblyinfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NoID Browser")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("NoID Thin ClientBrowser Using Chromium")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("HarmonIQ Health Systems and Ubiquitous Solutions")]
 [assembly: AssemblyProduct("NoID.Browser")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyCopyright("Copyright © 2016-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.4.5")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.17.05.20")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
