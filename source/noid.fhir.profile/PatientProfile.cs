7913e34918de6d17e956c68668c9aa8545bcea55
Amir Abrams <aabrams@myharmoniq.com>
2017-05-30 06:23:51 -0500

    [FHIR] Add Patient FHIR profile library

---
+++ source/noid.fhir.profile/PatientProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Hl7.Fhir.Rest;
+using Hl7.Fhir.Model;
+
+namespace NoID
+{
+    public class PatientProfile
+    {
+        private string _languageSelected = "";
+        private Patient pt = new Patient();
+
+        public string LanguageHash
+        {
+            get { return _languageSelected; }
+        }
+
+    }
+}
