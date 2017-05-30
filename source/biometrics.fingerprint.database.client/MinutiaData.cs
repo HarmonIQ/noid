53700fcb882602570b760d0359a365ee6824f89f
Amir Abrams <aabrams@myharmoniq.com>
2017-05-30 15:32:47 -0500

    [Biometrics] Add fingerprint minutia data struct library

---
+++ source/biometrics.fingerprint.database.client/MinutiaData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NoID.Biometrics.Fingerprint
+{
+    public enum MinutiaType : byte
+    {
+        Ending = 0,
+        Bifurcation = 1,
+        Other = 2
+    }
+
+    public struct Minutia
+    {
+        public short X;
+        public short Y;
+        public byte Direction;
+        public MinutiaType Type;
+    }
+}
