8537065db9ae06e0c2d81a014dc5a43956ff2a6c
Amir Abrams <aabrams@myharmoniq.com>
2017-06-29 15:23:50 -0500

    [Test] Improve FHIR endpoint testing application project

--- source/noid.network.client.test/Program.cs
+++ source/noid.network.client.test/Program.cs
@@ -14,14 +14,20 @@ namespace NoID.Network.Client.Test
     {
         private static readonly string FHIREndPoint = System.Configuration.ConfigurationManager.AppSettings["FHIREndPoint"].ToString();
         private static readonly string NoIDServiceName = System.Configuration.ConfigurationManager.AppSettings["NoIDServiceName"].ToString();
-        
+
         static void Main(string[] args)
         {
             string commandLine = "";
             Console.WriteLine("Enter q to Quit and Enter to try again.");
-            while (commandLine  != "q")
-            { 
-                SendJSON();
+            while (commandLine != "q")
+            {
+                Console.WriteLine("Sending test patient FHIR message.");
+                Patient testPt = TestPatient();
+                SendJSON(testPt);
+                Console.WriteLine("Sending FHIR message from file.");
+                Patient readPt = ReadPatient();
+                SendJSON(readPt);
+
                 // SendProtoBuffer();
                 commandLine = "";
                 commandLine = Console.ReadLine();
@@ -37,11 +43,24 @@ namespace NoID.Network.Client.Test
             return FHIRUtilities.FileToResource(@"C:\JSONTest\sample-new-patient.json");
         }
 
-        private static void SendJSON()
+        private static Patient ReadPatient()
         {
-            Patient payload = (Patient)ReadJSONFile();
-            //Patient payload = FHIRUtilities.CreateTestFHIRPatientProfile();
-            //FHIRUtilities.SaveJSONFile(payload, @"C:\JSONTest");
+            return (Patient)ReadJSONFile();
+        }
+
+        private static Patient TestPatient()
+        {
+            Patient testPatient = FHIRUtilities.CreateTestFHIRPatientProfile
+                (
+                "Test NoID Org", Guid.NewGuid().ToString(), "", "English", "1961-04-22", "F", "No",
+                "Donna", "Marie", "Kasick", "4712 W 3rd St.", "Apt 35", "New York", "NY", "10000-2221",
+                "212-555-3000", "212-555-7400", "212-555-9555", "donnakasick@yandex.com"
+                );
+            return testPatient;
+        }
+
+        private static void SendJSON(Patient payload)
+        {
             Authentication auth = SecurityUtilities.GetAuthentication(NoIDServiceName);
             Uri endpoint = new Uri(FHIREndPoint);
             HttpsClient client = new HttpsClient();
